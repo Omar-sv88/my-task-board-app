634e2cb64081a631efaaeecf4aba87344043b0df
Omar Santos <contacto@omarsantos.dev>
2025-11-06 11:23:21 +0100

    Add sqlite instead use postgres in backend

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -10,12 +10,8 @@ import { CommonModule } from './common/common.module';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRES_PORT),
-      database: process.env.POSTGRES_DB,
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
+      type: 'sqlite',
+      database: 'my-task-board.db',
       autoLoadEntities: true,
       synchronize: true,
     }),
